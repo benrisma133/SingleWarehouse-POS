949ec3d817050205cc57333fe70ef52e410c9148
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-09 22:13:25 +0100

    Add dashboard logic (BLL)

---
+++ POS_BLL/clsDashboard.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using POS_DAL;
+
+namespace POS_BLL
+{
+    public static class clsDashboard
+    {
+        public static int GetTotalProducts()
+            => clsDashboardData.GetTotalProducts();
+
+        public static int GetTotalSales()
+            => clsDashboardData.GetTotalSales();
+
+        public static decimal GetRevenue()
+            => clsDashboardData.GetTotalRevenue();
+
+        public static int GetLowStock(int threshold = 10)
+            => clsDashboardData.GetLowStockCount(threshold);
+
+        public static DataTable GetSalesChart()
+            => clsDashboardData.GetSalesLast7Days();
+
+        // Add to clsDashboard
+
+        public static DataTable GetLowStockItems(int threshold = 10)
+            => clsDashboardData.GetLowStockItems(threshold);
+
+        public static DataTable GetRecentSales(int limit = 10)
+            => clsDashboardData.GetRecentSales(limit);
+    }
+}
