8b7b2f9968326a894df51ce4b0bc882659ab3a10
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-10 22:06:13 +0100

    Update About Me page UI and logic

--- POS_WPF/Pages/AboutMePage.xaml.cs
+++ POS_WPF/Pages/AboutMePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,43 @@ namespace POS_WPF.Pages
         public AboutMePage()
         {
             InitializeComponent();
+        }
+
+        private void Email_Click(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("mailto:ibenrahhal133@gmail.com");
+        }
+
+        private void GitHub_Click(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("https://github.com/benrisma133");
+        }
+
+        private void LinkedIn_Click(object sender, MouseButtonEventArgs e)
+        {
+            Process.Start("https://www.linkedin.com/in/benrisma133");
+        }
+
+        private void WhatsApp_Click(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                // Opens WhatsApp chat directly in browser or WhatsApp app
+                string number = "212600144024"; // No + sign, no spaces
+                string url = $"https://wa.me/{number}";
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = url,
+                    UseShellExecute = true
+                });
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Could not open WhatsApp:\n{ex.Message}",
+                                "Error",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Warning);
+            }
         }
     }
 }
