ef36c51259e82cb9b9b96144805f77fb1309f9c6
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-11 23:47:07 +0100

    I finished code behind to be all topics closed.

--- POS_WPF/Pages/HelpPage.xaml.cs
+++ POS_WPF/Pages/HelpPage.xaml.cs
@@ -14,6 +14,10 @@ namespace POS_WPF.Pages
         private bool _modelsTopicOpen = false;
         private bool _seriesTopicOpen = false;
         private bool _brandsTopicOpen = false;
+        private bool _productsTopicOpen = false;
+        private bool _clientsTopicOpen = false;
+        private bool _newSaleTopicOpen = false;
+        private bool _saleDetailsTopicOpen = false;
 
         private bool _addOpen = false;
         private bool _updateOpen = false;
@@ -30,6 +34,18 @@ namespace POS_WPF.Pages
         private bool _addBrandOpen = false;
         private bool _updateBrandOpen = false;
         private bool _deleteBrandOpen = false;
+
+        private bool _addProductOpen = false;
+        private bool _updateProductOpen = false;
+        private bool _deleteProductOpen = false;
+
+        private bool _addClientOpen = false;
+        private bool _updateClientOpen = false;
+        private bool _deleteClientOpen = false;
+
+        private bool _howToSaleOpen = false;
+        private bool _viewSalesOpen = false;
+        private bool _deleteSaleOpen = false;
 
         public HelpPage()
         {
@@ -70,6 +86,38 @@ namespace POS_WPF.Pages
             if (!_brandsTopicOpen) CollapseSubItems_Brands();
             TogglePanel(BrandsTopicContent, BrandsTopicInner,
                         BrandsTopicArrow, _brandsTopicOpen);
+        }
+
+        private void ProductsTopic_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _productsTopicOpen = !_productsTopicOpen;
+            if (!_productsTopicOpen) CollapseSubItems_Products();
+            TogglePanel(ProductsTopicContent, ProductsTopicInner,
+                        ProductsTopicArrow, _productsTopicOpen);
+        }
+
+        private void ClientsTopic_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _clientsTopicOpen = !_clientsTopicOpen;
+            if (!_clientsTopicOpen) CollapseSubItems_Clients();
+            TogglePanel(ClientsTopicContent, ClientsTopicInner,
+                        ClientsTopicArrow, _clientsTopicOpen);
+        }
+
+        private void NewSaleTopic_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _newSaleTopicOpen = !_newSaleTopicOpen;
+            if (!_newSaleTopicOpen) CollapseSubItems_NewSale();
+            TogglePanel(NewSaleTopicContent, NewSaleTopicInner,
+                        NewSaleTopicArrow, _newSaleTopicOpen);
+        }
+
+        private void SaleDetailsTopic_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _saleDetailsTopicOpen = !_saleDetailsTopicOpen;
+            if (!_saleDetailsTopicOpen) CollapseSubItems_SaleDetails();
+            TogglePanel(SaleDetailsTopicContent, SaleDetailsTopicInner,
+                        SaleDetailsTopicArrow, _saleDetailsTopicOpen);
         }
 
         // ════════════════════════════════════════════════════════════
@@ -161,10 +209,84 @@ namespace POS_WPF.Pages
         }
 
         // ════════════════════════════════════════════════════════════
+        //  PRODUCT SUB-ITEMS
+        // ════════════════════════════════════════════════════════════
+
+        private void AddProduct_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _addProductOpen = !_addProductOpen;
+            TogglePanel(AddProductContent, AddProductInner, AddProductArrowRotate, _addProductOpen);
+        }
+
+        private void UpdateProduct_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _updateProductOpen = !_updateProductOpen;
+            TogglePanel(UpdateProductContent, UpdateProductInner, UpdateProductArrowRotate, _updateProductOpen);
+        }
+
+        private void DeleteProduct_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _deleteProductOpen = !_deleteProductOpen;
+            TogglePanel(DeleteProductContent, DeleteProductInner, DeleteProductArrowRotate, _deleteProductOpen);
+        }
+
+        // ════════════════════════════════════════════════════════════
+        //  CLIENT SUB-ITEMS
+        // ════════════════════════════════════════════════════════════
+
+        private void AddClient_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _addClientOpen = !_addClientOpen;
+            TogglePanel(AddClientContent, AddClientInner, AddClientArrowRotate, _addClientOpen);
+        }
+
+        private void UpdateClient_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _updateClientOpen = !_updateClientOpen;
+            TogglePanel(UpdateClientContent, UpdateClientInner, UpdateClientArrowRotate, _updateClientOpen);
+        }
+
+        private void DeleteClient_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _deleteClientOpen = !_deleteClientOpen;
+            TogglePanel(DeleteClientContent, DeleteClientInner, DeleteClientArrowRotate, _deleteClientOpen);
+        }
+
+        // ════════════════════════════════════════════════════════════
+        //  NEW SALE SUB-ITEMS
+        // ════════════════════════════════════════════════════════════
+
+        private void HowToSale_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _howToSaleOpen = !_howToSaleOpen;
+            TogglePanel(HowToSaleContent, HowToSaleInner, HowToSaleArrowRotate, _howToSaleOpen);
+        }
+
+        // ════════════════════════════════════════════════════════════
+        //  SALE DETAILS SUB-ITEMS
+        // ════════════════════════════════════════════════════════════
+
+        private void ViewSales_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _viewSalesOpen = !_viewSalesOpen;
+            TogglePanel(ViewSalesContent, ViewSalesInner, ViewSalesArrowRotate, _viewSalesOpen);
+        }
+
+        private void DeleteSale_Toggle(object sender, MouseButtonEventArgs e)
+        {
+            _deleteSaleOpen = !_deleteSaleOpen;
+            TogglePanel(DeleteSaleContent, DeleteSaleInner, DeleteSaleArrowRotate, _deleteSaleOpen);
+        }
+
+        // ════════════════════════════════════════════════════════════
         //  CORE TOGGLE
         // ════════════════════════════════════════════════════════════
 
-        private void TogglePanel(Border container ,FrameworkElement inner ,RotateTransform arrowRotate ,bool open)
+        private void TogglePanel(
+            Border container,
+            FrameworkElement inner,
+            RotateTransform arrowRotate,
+            bool open)
         {
             var duration = new Duration(TimeSpan.FromMilliseconds(320));
             var ease = new CubicEase { EasingMode = EasingMode.EaseInOut };
@@ -203,7 +325,6 @@ namespace POS_WPF.Pages
             else
             {
                 double fromH = container.ActualHeight;
-
                 var heightAnim = new DoubleAnimation(fromH, 0, duration)
                 { EasingFunction = ease };
                 container.BeginAnimation(HeightProperty, heightAnim);
@@ -225,7 +346,7 @@ namespace POS_WPF.Pages
         //  SILENT COLLAPSE
         // ════════════════════════════════════════════════════════════
 
-        private void SilentCollapse(Border container ,FrameworkElement inner ,RotateTransform arrow)
+        private void SilentCollapse(Border container, FrameworkElement inner, RotateTransform arrow)
         {
             container.BeginAnimation(HeightProperty, null);
             container.Height = 0;
@@ -261,6 +382,31 @@ namespace POS_WPF.Pages
             if (_addBrandOpen) { SilentCollapse(AddBrandContent, AddBrandInner, AddBrandArrowRotate); _addBrandOpen = false; }
             if (_updateBrandOpen) { SilentCollapse(UpdateBrandContent, UpdateBrandInner, UpdateBrandArrowRotate); _updateBrandOpen = false; }
             if (_deleteBrandOpen) { SilentCollapse(DeleteBrandContent, DeleteBrandInner, DeleteBrandArrowRotate); _deleteBrandOpen = false; }
+        }
+
+        private void CollapseSubItems_Products()
+        {
+            if (_addProductOpen) { SilentCollapse(AddProductContent, AddProductInner, AddProductArrowRotate); _addProductOpen = false; }
+            if (_updateProductOpen) { SilentCollapse(UpdateProductContent, UpdateProductInner, UpdateProductArrowRotate); _updateProductOpen = false; }
+            if (_deleteProductOpen) { SilentCollapse(DeleteProductContent, DeleteProductInner, DeleteProductArrowRotate); _deleteProductOpen = false; }
+        }
+
+        private void CollapseSubItems_Clients()
+        {
+            if (_addClientOpen) { SilentCollapse(AddClientContent, AddClientInner, AddClientArrowRotate); _addClientOpen = false; }
+            if (_updateClientOpen) { SilentCollapse(UpdateClientContent, UpdateClientInner, UpdateClientArrowRotate); _updateClientOpen = false; }
+            if (_deleteClientOpen) { SilentCollapse(DeleteClientContent, DeleteClientInner, DeleteClientArrowRotate); _deleteClientOpen = false; }
+        }
+
+        private void CollapseSubItems_NewSale()
+        {
+            if (_howToSaleOpen) { SilentCollapse(HowToSaleContent, HowToSaleInner, HowToSaleArrowRotate); _howToSaleOpen = false; }
+        }
+
+        private void CollapseSubItems_SaleDetails()
+        {
+            if (_viewSalesOpen) { SilentCollapse(ViewSalesContent, ViewSalesInner, ViewSalesArrowRotate); _viewSalesOpen = false; }
+            if (_deleteSaleOpen) { SilentCollapse(DeleteSaleContent, DeleteSaleInner, DeleteSaleArrowRotate); _deleteSaleOpen = false; }
         }
     }
 }
