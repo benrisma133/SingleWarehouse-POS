193133bdd806e5a806ca90b958f145087c288458
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-12 17:40:48 +0100

    feat(WPF): add Utils folder with clsUtil helper methods

---
+++ POS_WPF/Utils/clsUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS_WPF.Utils
+{
+    public static class clsUtil
+    {
+        public static string HashPassword(string password)
+        {
+            if (string.IsNullOrEmpty(password))
+                return null;
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(password);
+                byte[] hash = sha256.ComputeHash(bytes);
+
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in hash)
+                {
+                    builder.Append(b.ToString("x2")); // convert to hex
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
+}
