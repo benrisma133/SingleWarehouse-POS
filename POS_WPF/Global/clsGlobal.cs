35768d408475ab22c48c2fd96f6bf71ced2c320a
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-12 17:40:37 +0100

    feat(WPF): add Global folder with CurrentUser and credentials helpers

---
+++ POS_WPF/Global/clsGlobal.cs
@@ -0,0 +1,95 @@
+﻿using POS_BLL;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace POS_WPF.Global
+{
+    public static class clsGlobal
+    {
+        public static clsUser CurrentUser;
+
+        public static bool RememberUsernameAndPassword(string Username, string Password)
+        {
+            try
+            {
+                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+
+                string filePath = currentDirectory + "\\data.txt";
+
+                if (Username == "" && File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                    return true;
+                }
+
+                string dataToSave = Username + "#//#" + Password;
+
+                using (StreamWriter writer = new StreamWriter(filePath))
+                {
+                    writer.Write(dataToSave);
+
+                    return true;
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occured : {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        public static bool GetStoredCredential(ref string Username, ref string Password)
+        {
+
+            try
+            {
+                string currentDirectory = System.IO.Directory.GetCurrentDirectory();
+
+                string filePath = currentDirectory + "\\data.txt";
+
+                if (File.Exists(filePath))
+                {
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        string line;
+
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            Console.WriteLine(line);
+                            string[] result = line.Split(new string[] { "#//#" }, StringSplitOptions.None);
+
+                            Username = result[0];
+                            Password = result[1];
+
+
+                        }
+                        return true;
+
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occured : {ex.Message}");
+                return false;
+            }
+
+        }
+
+
+
+    }
+}
