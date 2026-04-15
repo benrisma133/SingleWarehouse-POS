462e6842d1a3a4704b771503e0f528c9324f342a
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-15 19:08:45 +0100

    Add confirm delete dialog logic

---
+++ POS_WPF/Dialogs/frmConfirmDelete.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace POS_WPF.Dialogs
+{
+    public partial class frmConfirmDelete : Window
+    {
+        public bool Confirmed { get; private set; } = false;
+
+        public frmConfirmDelete(string title, string message)
+        {
+            InitializeComponent();
+
+            TxtTitle.Text = title;
+            TxtMessage.Text = $"\"{message}\"";
+
+            MouseLeftButtonDown += (s, e) =>
+            {
+                if (e.ButtonState == MouseButtonState.Pressed)
+                    DragMove();
+            };
+        }
+
+        private void BtnDeleteAnyway_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
