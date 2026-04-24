fb3f6334b6f349a82622eb96d4825b6255d88fca
ismail benrahhal <ibenrahhal133@gmail.com>
2026-04-24 17:44:07 +0100

    chore(clsDataAccessSettings): update data access configuration

--- POS_DAL/clsDataAccessSettigs.cs
+++ POS_DAL/clsDataAccessSettigs.cs
@@ -10,22 +10,10 @@ namespace POS_DAL
     public static class clsDataAccessSettigs
     {
 
-        //public static string ConnectionString = @"Server=.;
-        //                                    Database=POS2;
-        //                                    User Id=sa;
-        //                                    Password=sa123456;
-        //                                    TrustServerCertificate=True;";
-
-        //public static string ConnectionString = @"Server=(localdb)\POS2LocalDB;
-        //                                 AttachDbFilename=|DataDirectory|\Data\POS2.mdf;
-        //                                 Integrated Security=True;
-        //                                 Connect Timeout=30;";
-
-        //    public static string ConnectionString =
-        //@"Data Source=C:\Users\DELL\Desktop\My Prog. Career Path\Projects\C#\WinForms\POS\POS_DAL\Data\POS.db;";
+        
 
         public static string ConnectionString =
-    $"Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "POS.db")};";
+                 $"Data Source={Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data", "POS.db")};";
 
 
 
